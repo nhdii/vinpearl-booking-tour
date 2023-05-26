351eb3bd00396867e09d7d0d4ed6a5cc89a9363d
Hoai Dii <hoaidii.it@gmail.com>
2023-05-26 11:00:33 +0700

    Vinpearl Booking Tour

---
+++ lib/service_data/meeting_data.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+class MeetingService {
+  List<dynamic> anh;
+  String chuU, cooktail, daTiec, dienTich, gia, lopHoc, maDV, moTa, nhaHat, tenDV;
+
+  MeetingService({
+    required this.anh,
+    required this.chuU,
+    required this.cooktail,
+    required this.daTiec,
+    required this.dienTich,
+    required this.gia,
+    required this.lopHoc,
+    required this.maDV,
+    required this.moTa,
+    required this.nhaHat,
+    required this.tenDV,
+  });
+  Map<String, dynamic> toJson() {
+    return {
+      'anh': anh,
+      'chuU': chuU,
+      'cooktail': cooktail,
+      'daTiec': daTiec,
+      'dienTich': dienTich,
+      'gia': gia,
+      'lopHoc': lopHoc,
+      'maDV': maDV,
+      'moTa': moTa,
+      'nhaHat': nhaHat,
+      'tenDV': tenDV,
+    };
+  }
+
+  factory MeetingService.fromJson(Map<String, dynamic> map) {
+    return MeetingService(
+      anh: map['anh'] as List<dynamic>,
+      chuU: map['chuU'] as String,
+      cooktail: map['cooktail'] as String,
+      daTiec: map['daTiec'] as String,
+      dienTich: map['dienTich'] as String,
+      gia: map['gia'] as String,
+      lopHoc: map['lopHoc'] as String,
+      maDV: map['maDV'] as String,
+      moTa: map['moTa'] as String,
+      nhaHat: map['nhaHat'] as String,
+      tenDV: map['tenDV'] as String,
+    );
+  }
+}
+
+class MeetingServiceSnapshot {
+  MeetingService meetingService;
+  DocumentReference documentReference;
+
+  MeetingServiceSnapshot({
+    required this.meetingService,
+    required this.documentReference,
+  });
+
+  factory MeetingServiceSnapshot.fromSnapshot(DocumentSnapshot docSnapMeetingService) {
+    return MeetingServiceSnapshot(
+      meetingService: MeetingService.fromJson(docSnapMeetingService.data() as Map<String, dynamic>),
+      documentReference: docSnapMeetingService.reference,
+    );
+  }
+
+  static Stream<List<MeetingServiceSnapshot>> listMeetingService()
+  {
+    Stream<QuerySnapshot> streamQS = FirebaseFirestore.instance.collection("MeetingService").snapshots();
+    Stream<List<DocumentSnapshot>> streamListDocSnap = streamQS.map(
+            (queryInfo) => queryInfo.docs);
+    return streamListDocSnap.map((listDS) => listDS.map((ds) => MeetingServiceSnapshot.fromSnapshot(ds)).toList()
+    );
+  }
+}
